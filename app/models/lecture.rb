0f17e0605f573184c9096eec1f40fd48b05f6c17
danhillcode <danhilltravel@hotmail.com>
2016-05-31 19:12:47 +0200

    update db

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -5,5 +5,6 @@ class Lecture < ActiveRecord::Base
   has_many :lecture_student, :dependent => :destroy
   has_many :students, through: :lecture_student
   belongs_to :teacher
+  validates_presence_of :teacher
 
 end
